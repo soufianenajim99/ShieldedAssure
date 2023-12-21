35d4cf66825e1d04adcd71ff58a0cafbbe7f5616
soufianenajim99 <soufianenajim100@gmail.com>
2023-12-21 16:06:56 +0100

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./*.html",
+    "./app/views/*.{html,php}",
+    "./app/views/admin/*.{html,php}",
+  ],
+  content: [
+    "./*.html",
+    "./app/views/*.{html,php}",
+    "./app/views/admin/*.{html,php}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), require("flowbite/plugin")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "autumn"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "autumn", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+};
